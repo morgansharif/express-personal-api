216e13525350046f58a246657fe19961806b1d9b
morgansharif <morgan.sharif@gmail.com>
2016-04-01 14:18:46 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
-                  "mongodb://localhost/personal-api");
+                  "mongodb://localhost/personal-api");  //"YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE"
 
 // module.exports.Campsite = require("./campsite.js.example");
